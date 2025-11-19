253838b29e02fcd71ed31629f7fdebfdf9bf0be2
jnt4vp <98505297+jnt4vp@users.noreply.github.com>
2025-11-18 23:55:39 -0600

    Update SosGame.py

--- SosGame.py
+++ SosGame.py
@@ -1,7 +1,74 @@
 from abc import ABC, abstractmethod
+import random
+
+class PlayerBase(ABC):
+    def __init__(self, name, piece_choice):
+        self.name = name
+        self.piece_choice = piece_choice
+
+    @abstractmethod
+    def get_move(self, game):
+        pass
+
+class HumanPlayer(PlayerBase):
+    def get_move(self, game):
+        return None, None, None
+
+class ComputerPlayer(PlayerBase):
+    def get_move(self, game):
+        empty_cells = []
+        for r in range(game.board_size):
+            for c in range(game.board_size):
+                if game.board[r][c] == '':
+                    empty_cells.append((r, c))
+
+        if not empty_cells:
+            return None, None, None
+
+        for r, c in empty_cells:
+            for piece in ['S', 'O']:
+                temp_board = [row[:] for row in game.board]
+                temp_board[r][c] = piece
+                soses = self._check_for_sos_temp(r, c, piece, temp_board, game.board_size)
+                if soses:
+                    return r, c, piece
+
+        r, c = random.choice(empty_cells)
+        piece = self.piece_choice
+            
+        return r, c, piece
+
+    def _check_for_sos_temp(self, r, c, piece, board, board_size):
+        found_soses = []
+        directions = [(0, 1), (1, 0), (1, 1), (1, -1)]
+
+        def _is_valid(r_val, c_val):
+            return 0 <= r_val < board_size and 0 <= c_val < board_size
+
+        if piece == 'O':
+            for dr, dc in directions:
+                if _is_valid(r - dr, c - dc) and board[r - dr][c - dc] == 'S' and \
+                   _is_valid(r + dr, c + dc) and board[r + dr][c + dc] == 'S':
+                    found_soses.append(((r - dr, c - dc), (r + dr, c + dc)))
+                    
+        elif piece == 'S':
+            for dr, dc in directions:
+                r_o, c_o = r + dr, c + dc
+                r_s, c_s = r + 2*dr, c + 2*dc
+                if _is_valid(r_s, c_s) and board[r_s][c_s] == 'S' and \
+                   _is_valid(r_o, c_o) and board[r_o][c_o] == 'O':
+                    found_soses.append(((r, c), (r_s, c_s)))
+
+                r_o, c_o = r - dr, c - dc
+                r_s, c_s = r - 2*dr, c - 2*dc
+                if _is_valid(r_s, c_s) and board[r_s][c_s] == 'S' and \
+                   _is_valid(r_o, c_o) and board[r_o][c_o] == 'O':
+                    found_soses.append(((r_s, c_s), (r, c)))
+        
+        return found_soses
 
 class SOSGameBase(ABC):
-    def __init__(self, board_size):
+    def __init__(self, board_size, blue_player, red_player):
         if board_size < 3:
             raise ValueError("Board size must be at least 3")
         self.board_size = board_size
@@ -9,6 +76,11 @@ class SOSGameBase(ABC):
         self.current_turn_is_blue = True
         self.game_over = False
         self.winner = None
+        self.blue_player = blue_player
+        self.red_player = red_player
+
+    def get_current_player(self):
+        return self.blue_player if self.current_turn_is_blue else self.red_player
 
     def _is_valid(self, r, c):
         return 0 <= r < self.board_size and 0 <= c < self.board_size
@@ -32,11 +104,17 @@ class SOSGameBase(ABC):
                     
         elif piece == 'S':
             for dr, dc in directions:
-                for mult in [-1, 1]:
-                    dr_m, dc_m = dr * mult, dc * mult
-                    if self._is_valid(r + dr_m, c + dc_m) and self.board[r + dr_m][c + dc_m] == 'O' and \
-                       self._is_valid(r + 2*dr_m, c + 2*dc_m) and self.board[r + 2*dr_m][c + 2*dc_m] == 'S':
-                        found_soses.append(((r, c), (r + 2*dr_m, c + 2*dc_m)))
+                r_o, c_o = r + dr, c + dc
+                r_s, c_s = r + 2*dr, c + 2*dc
+                if self._is_valid(r_s, c_s) and self.board[r_s][c_s] == 'S' and \
+                   self._is_valid(r_o, c_o) and self.board[r_o][c_o] == 'O':
+                    found_soses.append(((r, c), (r_s, c_s)))
+
+                r_o, c_o = r - dr, c - dc
+                r_s, c_s = r - 2*dr, c - 2*dc
+                if self._is_valid(r_s, c_s) and self.board[r_s][c_s] == 'S' and \
+                   self._is_valid(r_o, c_o) and self.board[r_o][c_o] == 'O':
+                    found_soses.append(((r_s, c_s), (r, c)))
         
         return found_soses
 
@@ -45,11 +123,11 @@ class SOSGameBase(ABC):
         pass
 
     def get_turn_owner_name(self):
-        return "Blue" if self.current_turn_is_blue else "Red"
+        return self.get_current_player().name
 
 class SimpleGame(SOSGameBase):
-    def __init__(self, board_size):
-        super().__init__(board_size)
+    def __init__(self, board_size, blue_player, red_player):
+        super().__init__(board_size, blue_player, red_player)
 
     def make_move(self, r, c, piece):
         if self.game_over or not self._is_valid(r, c) or self.board[r][c] != '':
@@ -72,8 +150,8 @@ class SimpleGame(SOSGameBase):
         return [], True
 
 class GeneralGame(SOSGameBase):
-    def __init__(self, board_size):
-        super().__init__(board_size)
+    def __init__(self, board_size, blue_player, red_player):
+        super().__init__(board_size, blue_player, red_player)
         self.blue_score = 0
         self.red_score = 0
 
@@ -84,7 +162,9 @@ class GeneralGame(SOSGameBase):
         self.board[r][c] = piece
         soses_found = self._check_for_sos(r, c, piece)
         
-        if soses_found:
+        scored = bool(soses_found)
+        
+        if scored:
             points = len(soses_found)
             if self.current_turn_is_blue:
                 self.blue_score += points
